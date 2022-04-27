b2c3afd114b9d82c9de876d0b28b268750baa85e
Ashish <ash242242@gmail.com>
2022-04-27 22:31:55 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (23.34%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,14 +1,29 @@
 class Solution {
-    
+//     Memoization approach dp 
     public int steps(int[] dp,int n){
         if(n<=1) return 1;
         if(dp[n]!=0) return dp[n];
         return dp[n]=steps(dp,n-1)+steps(dp,n-2);
         
     }
+    
+      public int steps2(int[] dp,int n){
+      for(int i=0;i<n;i++){
+            if(n<=1) {
+                dp[n]= 1;
+                continue;
+            }
+       
+       dp[n]=steps(dp,n-1)+steps(dp,n-2);
+      }
+          return dp[n];
+        
+    }
+    
     public int climbStairs(int n) {
         int []dp=new int[n+1];
-        int x= steps(dp,n);
+        // int x= steps(dp,n);
+        int x=steps2(dp,n);
         return x;
         
     }
