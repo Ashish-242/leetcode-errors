9e8e6103515b10b4ce15d187a7642dfa02a0d56d
Ashish <ash242242@gmail.com>
2022-05-05 15:32:14 +0530

    Added solution - LeetHub

---
+++ Friends Pairing Problem - GFG/friends-pairing-problem.java
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.countFriendsPairings(n));
+                }
+        }
+}    // } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+  
+ public long friendspairing_long(int  n,long[] dp)
+{
+      long mod =(long)1e9 + 7;
+       for(int i =0;i<=n;i++){
+           if(i<=1){
+               dp[i]=1;
+               continue;
+           }
+           dp[i]=(dp[i-1]%mod+(dp[i-2]%mod*(i-1))%mod)%mod;
+       }
+       
+       return dp[n];
+   } 
+  public  long countFriendsPairings(int n)
+    {
+        long[] dp=new long[n+1];
+    long ans = friendspairing_long(n,dp);
+    return ans;
+    }
+}    
+ 
