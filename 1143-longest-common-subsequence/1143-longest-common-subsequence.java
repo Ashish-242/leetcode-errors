268e615c88314aacb3229de6bfa5dc05d2f82d3f
Ashish <ash242242@gmail.com>
2022-05-08 01:08:28 +0530

    Time: 20 ms (42.00%), Space: 47.3 MB (10.06%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,18 +1,33 @@
 class Solution {
-    public int longestcommonsubsequence_memo(String s1,String s2,int n,int m, int[][] dp){
-        if(n==0 || m==0) return dp[n][m]=0;
-        if(dp[n][m]!=-1) return dp[n][m];
-        
-        int a=longestcommonsubsequence_memo(s1,s2,n-1,m-1,dp);
-        int b=longestcommonsubsequence_memo(s1,s2,n-1,m,dp);
-        int c=longestcommonsubsequence_memo(s1,s2,n,m-1,dp);
-        
-        if(s1.charAt(n-1) == s2.charAt(m-1)){
-            dp[n][m]=a+1;
-        }else{
-            dp[n][m]=Math.max(b,c);
+    
+    // public void display2d(int[][] dp){
+    //     for(int i=0;i<dp.length;i++){
+    //         for(int j =0;j<dp[0].length;j++){
+    //             System.out.print(dp[i][j]);
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+    
+  public int longestcommonsubsequence_memo(String s1,String s2,int N,int M, int[][] dp){
+       
+        for(int n=0;n<=N;n++){
+            for(int m=0;m<=M;m++){
+      if(n==0 && m==0 || (n==0 || m==0)) {
+            dp[n][m]=0;
+            continue;
         }
-        return dp[n][m];
+    
+        if(s1.charAt(n-1) == s2.charAt(m-1)){
+            dp[n][m]=1 + dp[n-1][m-1];
+        }else{
+            dp[n][m]=Math.max(dp[n-1][m],dp[n][m-1]);
+        }
+    
+            }
+        }
+        return dp[N][M];
+   
         
         
     }
@@ -21,35 +36,11 @@ class Solution {
         int n = s1.length();
         int m =s2.length();
         int[][] dp = new int[n+1][m+1];
-        for(int[] d: dp){
-            Arrays.fill(d,-1);
-        }
+        // display2d(dp);
+       
         int x =longestcommonsubsequence_memo(s1,s2,n,m,dp);
         return x;
     }
 }
 
 
-
-//   public int longestcommonsubsequence_memo(String s1,String s2,int N,int M, int[][] dp){
-       
-//         for(int n=N;n>=0;n--){
-//             for(int m=M;m>=0;m--){
-//                   if(n==0 || m==0) {
-//             dp[n][m]=0;
-//             continue;
-//         }
-    
-//         if(s1.charAt(n-1) == s2.charAt(m-1)){
-//             dp[n][m]=dp[n-1][m-1]+1;
-//         }else{
-//             dp[n][m]=Math.max(dp[n-1][m],dp[n][m-1]);
-//         }
-    
-//             }
-//         }
-//         return dp[N][M];
-   
-        
-        
-//     }
