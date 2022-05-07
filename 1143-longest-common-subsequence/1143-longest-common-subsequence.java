aaa9bda9bd005cf9ed0be34dcd69e83393d96456
Ashish <ash242242@gmail.com>
2022-05-08 00:13:04 +0530

    Time: 25 ms (26.63%), Space: 45.8 MB (91.44%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,7 +1,7 @@
 class Solution {
     public int longestcommonsubsequence_memo(String s1,String s2,int n,int m, int[][] dp){
         if(n==0 || m==0) return dp[n][m]=0;
-        if(dp[n][m]!=0) return dp[n][m];
+        if(dp[n][m]!=-1) return dp[n][m];
         
         int a=longestcommonsubsequence_memo(s1,s2,n-1,m-1,dp);
         int b=longestcommonsubsequence_memo(s1,s2,n-1,m,dp);
@@ -21,7 +21,35 @@ class Solution {
         int n = s1.length();
         int m =s2.length();
         int[][] dp = new int[n+1][m+1];
+        for(int[] d: dp){
+            Arrays.fill(d,-1);
+        }
         int x =longestcommonsubsequence_memo(s1,s2,n,m,dp);
         return x;
     }
 }
+
+
+
+//   public int longestcommonsubsequence_memo(String s1,String s2,int N,int M, int[][] dp){
+       
+//         for(int n=N;n>=0;n--){
+//             for(int m=M;m>=0;m--){
+//                   if(n==0 || m==0) {
+//             dp[n][m]=0;
+//             continue;
+//         }
+    
+//         if(s1.charAt(n-1) == s2.charAt(m-1)){
+//             dp[n][m]=dp[n-1][m-1]+1;
+//         }else{
+//             dp[n][m]=Math.max(dp[n-1][m],dp[n][m-1]);
+//         }
+    
+//             }
+//         }
+//         return dp[N][M];
+   
+        
+        
+//     }
