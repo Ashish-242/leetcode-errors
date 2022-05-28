19f742eb6faa509fdd5af539ba32bea0be6f4f19
Ashish <ash242242@gmail.com>
2022-05-29 04:25:36 +0530

    Time: 49 ms (8.85%), Space: 24.2 MB (5.40%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         if(nums.size()==0) return 0;
-        if(nums.size()==1 && nums[0]!=1){
-            return 1;
-        }
+        // if(nums.size()==1 && nums[0]!=1){
+        //     return 1;
+        // }
         unordered_map<int,int> mp;
         for(int val : nums){
             mp[val]++;
@@ -12,6 +12,7 @@ public:
         }
         for(int i=0;i<=nums.size();i++){
             if(mp.find(i)==mp.end()){
+//                 AGAR NAHI MILA TO YE CHALEGA
                 return i;
             }
         }
