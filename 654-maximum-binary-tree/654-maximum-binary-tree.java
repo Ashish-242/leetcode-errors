29f1f835038610643e04fcea48c73ed2df3ecc00
Ashish <ash242242@gmail.com>
2022-05-28 14:58:50 +0530

    Time: 5 ms (30.18%), Space: 54 MB (47.84%) - LeetHub

---
+++ 654-maximum-binary-tree/654-maximum-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode construct(int[] nums,int s,int e){
+        if(s>e) return null;
+        int max=Integer.MIN_VALUE;
+        int index=0;
+        for(int i=s;i<=e;i++){
+            if(nums[i]> max) index=i;
+            max=Math.max(nums[i],max);
+        }
+        TreeNode node=new TreeNode(max);
+        node.left=construct(nums,s,index-1);
+        node.right=construct(nums,index+1,e);
+        return node;
+    }
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        int n=nums.length;
+        TreeNode root = construct(nums,0,n-1);
+        return root;
+    }
+}
