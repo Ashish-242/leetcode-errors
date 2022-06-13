a1dd4a8a2a931a1a6c89938bcad3718fc2cb4e0d
Ashish <ash242242@gmail.com>
2022-06-13 22:09:54 +0530

    Time: 7 ms (31.43%), Space: 57.4 MB (40.99%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count=0;
+       boolean[][] visited=new boolean[grid.length][grid[0].length];
+        for(int i =0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1' && visited[i][j]==false){
+                    count++;
+                    traverse(grid,visited,i,j);
+                }
+            }
+        }
+        return count;
+    }
+    public void traverse(char[][] grid, boolean[][] visited,int i,int j){
+        if(i<0 || j<0  || i>=grid.length || j>=grid[0].length){
+            return;
+        }else if(visited[i][j]==true) return;
+        else if(grid[i][j]=='0') return;
+        
+        visited[i][j]=true;
+        
+        traverse(grid,visited,i-1,j);
+        traverse(grid,visited,i+1,j);
+        traverse(grid,visited,i,j-1);
+        traverse(grid,visited,i,j+1);
+        
+    }
+   
+}
