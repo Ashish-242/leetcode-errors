b6c5700c693e6b2e76480fac7a43710e08b9aaf2
Ashish <ash242242@gmail.com>
2022-06-23 14:45:45 +0530

    Time: 6 ms (58.65%), Space: 44.8 MB (41.57%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -17,3 +17,28 @@ class Solution {
         return pq.remove();
     }
 }
+// c++
+// c++ mei add ki jagah push hota h java mei add hota  h
+// c++ mei top hota h or java mei peek hota  h
+// c++ mei pop hota h or java mei remove hota h 
+
+
+    // int i=0;
+    // int findKthLargest(vector<int>& nums, int k) {
+    //     priority_queue<int,vector<int>,greater<int>> pq;
+    //     while(i!=k)
+    //     {
+    //         pq.push(nums[i]);
+    //         i++;
+    //     }
+    //     for(int i=k;i<nums.size();i++)
+    //     {
+    //         if(nums[i] > pq.top())
+    //         {
+    //             pq.pop();
+    //             pq.push(nums[i]);
+    //         }
+    //     }
+    //    return pq.top();
+        
+  
