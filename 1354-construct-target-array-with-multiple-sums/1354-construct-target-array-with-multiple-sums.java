0942376b56cdefe986118650cc3ab08985679e62
Ashish <ash242242@gmail.com>
2022-06-25 01:05:56 +0530

    Time: 18 ms (23.96%), Space: 57.3 MB (71.87%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -1,21 +1,52 @@
+// class Solution {
+//     public boolean isPossible(int[] target) {
+//         long sum=0;
+//         int max=0;
+        
+//         for(int i=0;i<target.length;i++){
+//             sum+=target[i];
+//             if(target[i]>target[max]) max=i;
+//         }
+//         long diff=sum-target[max];
+//         if(target[max]==1 || diff==1) return true;
+// //         [1,1000] diff==1
+// //         [1,1,1,1,1] max==1
+// //         [1,2,1] =diff==0
+// //         [1,2,2] diff>target[max];
+// //         target[max]%diff==0  [20,5]
+//         if(diff==0 || diff> target[max] || target[max]%diff==0 ) return false;
+        
+//         target[max]%=diff;
+//         return isPossible(target);
+//     }
+// }
+
+// Another Approach
 class Solution {
     public boolean isPossible(int[] target) {
         long sum=0;
-        int max=0;
+    
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
         for(int i=0;i<target.length;i++){
             sum+=target[i];
-            if(target[i]>target[max]) max=i;
+           pq.add(target[i]);
         }
-        long diff=sum-target[max];
-        if(target[max]==1 || diff==1) return true;
+        while(pq.peek()!=1){
+            int val=pq.poll();
+            long diff=sum-val;
+        if( diff==1) return true;
 //         [1,1000] diff==1
 //         [1,1,1,1,1] max==1
 //         [1,2,1] =diff==0
 //         [1,2,2] diff>target[max];
 //         target[max]%diff==0  [20,5]
-        if(diff==0 || diff> target[max] || target[max]%diff==0 ) return false;
-        
-        target[max]%=diff;
-        return isPossible(target);
+        if(diff==0 || diff> val || val%diff==0 ) return false;
+        sum=
+       val%=diff;
+            sum=val+diff;
+            pq.add(val);
+        }
+      
+        return true;
     }
 }
