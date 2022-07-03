d4847a9eb787962a05c9e24fab4c324249ba0057
Ashish <ash242242@gmail.com>
2022-07-03 21:08:28 +0530

    Time: 35 ms (46.14%), Space: 43.2 MB (76.60%) - LeetHub

--- 839-similar-string-groups/839-similar-string-groups.java
+++ 839-similar-string-groups/839-similar-string-groups.java
@@ -2,7 +2,7 @@ class Solution {
     public int numSimilarGroups(String[] str) {
         HashSet<String> visited=new HashSet<>();
         int ans=0;
-      
+
         for(String s:str){
             if(!visited.contains(s)){
                 dfs(s,str,visited);
@@ -15,6 +15,8 @@ class Solution {
     public void dfs(String s,String[] str,  HashSet<String> visited){
         if(visited.contains(s)) return;
         visited.add(s);
+//         yaha pr hum s ke index se aage ki taraf nahi dekhenge kunki aisa krne pr ho sta h ki jese    a b c h to a c se match kr ra h pr a b se matchnahi kr ra to a apne ap ko or c ko visited mei dal kr or c pr call chli jaygi ab c ke aage walo index ke liye chlayenge to koi ni milega lekin hum yaha ye bhool gye ki c to match kr ra tha b se to agr hum starting se cal lgate hmesa to c starting se chalta or b ko bhi visited kr deta isliye hum starting se call krenge
+//         agr hum bs forward call lgate index+1 se to b pr jb bat aati to use koi nimilta sath ka kyunki c pehle se hi set mei tha to wo alag componentho kr apne aap ko alag se add krwa lega
         for(int i=0;i<str.length;i++){
             if(issimiliar(s,str[i])){
                 dfs(str[i],str,visited);
