8993f63673371a58ed08ccdb9b3f34418809d9ad
Ashish <ash242242@gmail.com>
2022-07-13 12:12:29 +0530

    Time: 12 ms (78.95%), Space: 46.3 MB (37.58%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.java
+++ 778-swim-in-rising-water/778-swim-in-rising-water.java
@@ -1,5 +1,6 @@
 class Solution {
-    class Pair implements Comparable<Pair>{
+
+     class Pair implements Comparable<Pair>{
         int i;
         int j;
         int t;
@@ -12,31 +13,42 @@ class Solution {
             return this.t-o.t;
         }
     }
+    int m;
+    int n;
     public int swimInWater(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int time=0;
-     boolean[][] visited=new boolean[m][n];
+       
         PriorityQueue<Pair> pq=new PriorityQueue<>();
+        boolean[][] vis=new boolean[m][n];
         pq.add(new Pair(0,0,grid[0][0]));
-        
         while(pq.size()>0){
+//             remove
             Pair rem=pq.remove();
-            if(visited[rem.i][rem.j]==true) continue;
-            visited[rem.i][rem.j]=true;
-           
+            if(vis[rem.i][rem.j]==true) continue;
+            vis[rem.i][rem.j]=true;
+            
             if(rem.i==m-1 && rem.j==n-1){
                 return rem.t;
+            
             }
+            
 //             calls
-            solve(rem.i-1,rem.j,rem.t,grid,visited,pq);
-            solve(rem.i+1,rem.j,rem.t,grid,visited,pq);
-            solve(rem.i,rem.j+1,rem.t,grid,visited,pq);
-            solve(rem.i,rem.j-1,rem.t,grid,visited,pq);
+            solve(rem.i,rem.j+1,vis,rem.t,grid,pq);
+            solve(rem.i-1,rem.j,vis,rem.t,grid,pq);
+            solve(rem.i,rem.j-1,vis,rem.t,grid,pq);
+            solve(rem.i+1,rem.j,vis,rem.t,grid,pq);
+            
         }
         return -1;
     }
-    public void solve(int i,int j,int t,int[][] grid,boolean[][] visited,PriorityQueue<Pair> pq){
+    // public void solve(int x,int y,boolean[][] visited,int t,int[][] grid,PriorityQueue<Pair> pq){
+    //     if(x<0 || y<0 || visited[x][y]==true || x>=m || y>=n){
+    //         return;
+    //     }
+    //    pq.add(new Pair(x,y,Math.max(grid[x][y],t)));
+    // }
+    public void solve(int i,int j,boolean[][] visited,int t,int[][] grid,PriorityQueue<Pair> pq){
         if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || visited[i][j]==true){
             return;
         }
