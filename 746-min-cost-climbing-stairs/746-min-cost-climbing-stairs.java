4e4825e7b6021f2208b3da78fad3447edfeaa42e
Ashish <ash242242@gmail.com>
2022-07-15 18:32:58 +0530

    Time: 1 ms (89.43%), Space: 41.9 MB (92.18%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,36 +1,19 @@
 class Solution {
-//     public int min(int[] cost,int n,int[] dp){
-//         if(n<=1){
-//            return  dp[n]=cost[n];
-            
-//         }
-//         if(dp[n]!=0) return dp[n];
-//         int a=min(cost,n-1,dp);/*two calls wre made for n-1 and n-2*/
-//            int b=min(cost,n-2,dp);
-// //         we calculate minimum of both of calls and add theri corresponding value from cost array
-//        return dp[n] =Math.min(a,b) + (n==cost.length?0:cost[n]);
-       
-//     }
-//    BY Tabulaton 
-     public int min(int[] cost,int n,int[] dp){
-       for(int i=0;i<=n;i++){
-           if(i<=1){
-            dp[i]=cost[i];
-            continue;
-            
-        }
-     
-        int a=dp[i-1];/*two calls wre made for n-1 and n-2*/
-           int b=dp[i-2];
-//         we calculate minimum of both of calls and add theri corresponding value from cost array
-    dp[i] =Math.min(a,b) + (i==cost.length?0:cost[i]);
-       }
-         return dp[n];
-       
-    }
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp =new int[cost.length+1];
-        int x=min(cost,cost.length,dp);
+        int N=cost.length;
+        int[] dp=new int[N+1];
+        Arrays.fill(dp,-1);
+        int x=fib(cost,dp,N);
         return x;
+    }
+    public int fib(int[] cost,int[] dp,int N){
+        if(N<=1){
+            return dp[N]=cost[N];
+        }
+        if(dp[N]!=-1){
+            return dp[N];
+        }
+        
+        return dp[N]=Math.min(fib(cost,dp,N-1),fib(cost,dp,N-2))+ (N==dp.length-1?0:cost[N]);
     }
 }
