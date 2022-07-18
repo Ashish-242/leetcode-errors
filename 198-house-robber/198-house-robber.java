104a8af4e29c210d10884b2f17576118ee0ea0d7
Ashish <ash242242@gmail.com>
2022-07-18 13:39:28 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (24.17%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -3,20 +3,41 @@ class Solution {
         int n=nums.length;
         int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
-        int ans=mem(n-1,nums,dp);
+        // int ans=mem(n-1,nums,dp);
+        int ans=tabu(n,nums,dp);
         
         
         return ans;
     }
+//     int x=0;
+//     public int mem(int n,int[] arr,int[] dp){
+//        if(n==0) return dp[n]=arr[n];
+//         if(n<0) return 0;
+//         if(dp[n]!=-1) return dp[n];
+//         int pick=arr[n]+mem(n-2,arr,dp);
+//         int npick=mem(n-1,arr,dp);
+//         dp[n]=Math.max(pick,npick);
+//         return dp[n];
+        
+//     }
+     // }
     int x=0;
-    public int mem(int n,int[] arr,int[] dp){
-       if(n==0) return dp[n]=arr[n];
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int pick=arr[n]+mem(n-2,arr,dp);
-        int npick=mem(n-1,arr,dp);
+    public int tabu(int N,int[] arr,int[] dp){
+        for(int n=0;n<N;n++){
+            if(n==0) {
+               dp[n]=arr[0];
+                continue;
+            } 
+       
+        
+        int pick=arr[n];
+        if(n-2>=0) pick+=dp[n-2];
+        int npick=dp[n-1];
         dp[n]=Math.max(pick,npick);
-        return dp[n];
+        
+        }
+        return dp[N-1];
+
         
     }
 }
