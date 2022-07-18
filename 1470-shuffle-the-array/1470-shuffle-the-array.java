67265d6d9d6f1fc816ee635c285817aa8176de4e
Ashish <ash242242@gmail.com>
2022-07-18 16:25:16 +0530

    Time: 1 ms (63.12%), Space: 45.6 MB (61.26%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -3,11 +3,12 @@ class Solution {
         int[] arr=new int[nums.length];
        for(int i=0,x=0;i<n;i++,x=x+2){
            arr[x]=nums[i];
+           arr[x+1]=nums[i+n];
        }
-        for(int j=n,y=1;j<2*n;j++,y=y+2)
-        {
-            arr[y]=nums[j];
-        }
+        // for(int j=n,y=1;j<2*n;j++,y=y+2)
+        // {
+        //     arr[y]=nums[j];
+        // }
         return arr;
     }
 }
