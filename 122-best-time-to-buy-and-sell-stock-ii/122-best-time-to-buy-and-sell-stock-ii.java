d0c37d4b52faa708702f87cbb5688d4d238eaf2f
Ashish <ash242242@gmail.com>
2022-08-02 15:49:57 +0530

    Time: 1 ms (97.55%), Space: 44.4 MB (44.93%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,24 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int[][] dp=new int[n][2];
-        for(int[] ar:dp){
-            Arrays.fill(ar,-1);
+         if (prices == null || prices.length <= 1) {
+            return 0;
         }
-         
-        int ans=solve(0,1,prices,n,dp);
-        return ans;
-    }
-    public int solve(int ind,int buy,int[] p,int n,int[][] dp){
-        if(ind==n) return 0;
-   
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-             int profit=0;
-        if(buy==1){
-           profit+=Math.max(-1*p[ind]+solve(ind+1,0,p,n,dp),solve(ind+1,1,p,n,dp)); 
-        }else{
-            profit+=Math.max(p[ind]+solve(ind+1,1,p,n,dp),solve(ind+1,0,p,n,dp));
+
+        int totalProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            // Checking if we can profit with previous day's price.
+            // If yes, then we buy on previous day and sell on current day.
+            // Add all such profits to get the total profit.
+            if (prices[i - 1] < prices[i]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
         }
-        return dp[ind][buy]= profit;
+
+        return totalProfit;
+    
     }
 }
