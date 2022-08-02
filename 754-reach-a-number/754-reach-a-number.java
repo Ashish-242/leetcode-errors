760fc68037f55a7a66b8ab1d0fbc05cd4111f3b2
Ashish <ash242242@gmail.com>
2022-08-02 19:05:52 +0530

    Time: 5 ms (23.09%), Space: 41.1 MB (26.08%) - LeetHub

---
+++ 754-reach-a-number/754-reach-a-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reachNumber(int target) {
+        target=Math.abs(target);
+        int x=0;
+        while(true){
+            int range=x*(x+1)/2;
+            if(range>=target && target%2==range%2) break;
+            x++;
+        }
+        return x;
+    }
+}
