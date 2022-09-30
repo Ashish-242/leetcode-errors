eb50306e9a47561797b76be7806143b2e3b6ebed
Ashish <ash242242@gmail.com>
2022-09-30 17:17:55 +0530

    Time: 86 ms (11.10%), Space: 120.1 MB (5.98%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.java
+++ 650-2-keys-keyboard/650-2-keys-keyboard.java
@@ -1,14 +1,24 @@
 class Solution {
     public int minSteps(int n) {
-        int ans=0;
-      for(int i=2;i*i<=n;){
-          if(n%i==0){
-              ans+=i;
-              n=n/i;
-          }else i++;
-              
-      }
-        if(n!=1) ans+=n;
+        if(n==1) return 0;
+        int[][] dp=new int[n+1][n+1];
+        for(int[] d:dp) Arrays.fill(d,-1);
+        int ans=solve(1,1,1,n,dp);
         return ans;
+        
+    }
+    public int solve(int step,int value,int copy,int n,int[][] dp){
+//         there are two steps paste copy value
+//         copy it and then paste it
+        if(step > n || value>n) return Integer.MAX_VALUE;
+        if(value==n) return step;
+        if(dp[step][value]!=-1) return dp[step][value];
+        
+        int minstep=Math.min(solve(step+1,value+copy,copy,n,dp),solve(step+2,value*2,value,n,dp));
+         return dp[step][value]=minstep;
+        
+        
     }
 }
+
+
