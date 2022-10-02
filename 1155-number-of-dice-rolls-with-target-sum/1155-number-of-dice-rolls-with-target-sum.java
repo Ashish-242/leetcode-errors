47c41f9d9a70761d43b49033b248c0eeeb9b3d93
Ashish <ash242242@gmail.com>
2022-10-02 18:15:48 +0530

    Time: 33 ms (56.11%), Space: 42.2 MB (70.51%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,12 +1,12 @@
 class Solution {
     public int numRollsToTarget(int n, int k, int target) {
-          dp=new Integer[n+1][target+1];
-        // for(int[] d:dp) Arrays.fill(d,-1);
+          dp=new int[n+1][target+1];
+        for(int[] d:dp) Arrays.fill(d,-1);
         
         return noofdiceroll(n,k,target);
         
     }
-    Integer[][] dp;
+    int[][] dp;
     int mod=(int)(1e9+7);
     public int noofdiceroll(int n,int k,int target){
         if(n==0){
@@ -14,11 +14,11 @@ class Solution {
             
         }
         if(target<0 || n<=0) return 0;
-        if(dp[n][target]!=null) return dp[n][target];
+        if(dp[n][target]!=-1) return dp[n][target];
         
         int take=0;
         for(int i=1;i<=k;i++){
-            take=(take + (noofdiceroll(n-1,k,target-i)))%mod;
+            take= (take%mod+ (noofdiceroll(n-1,k,target-i))%mod)%mod;
         }
         return dp[n][target]=take;
     }
