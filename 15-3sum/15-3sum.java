5ba187b6a4e444abb035559458c17fdaabbf2d49
Ashish <ash242242@gmail.com>
2022-10-08 11:42:26 +0530

    Time: 1414 ms (7.08%), Space: 118.9 MB (20.38%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -4,30 +4,21 @@ class Solution {
         Set<List<Integer>> set=new HashSet<>();
         int i=0;
         int n=nums.length;
-        List<List<Integer>> res=new ArrayList<>();
+        
         while(i<n-2){
             int j=i+1; 
             int k=n-1;
-            
             while(j<k){
-                List<Integer> r=new ArrayList<>();
                 int sum=nums[i]+nums[j]+nums[k];
                 if(sum==0){
-                    r.add(nums[i]);
-                    r.add(nums[j]);
-                    r.add(nums[k]);
-                    if(!set.contains(r)) {
-                        res.add(r);
-                        set.add(r);
-                    } 
-                  
+                set.add(Arrays.asList(nums[i],nums[j++],nums[k--])); 
                 }
                  if(sum>0) k--;
-                else j++;
+                else if(sum<0) j++;
                 
             }
             i++;
         }
-        return res;
+        return new ArrayList<>(set);
     }
 }
