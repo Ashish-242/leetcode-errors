583e22ada9d8f1d82856f9ca7001de7dc5131d27
Ashish <ash242242@gmail.com>
2022-10-10 06:55:31 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (6.04%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,42 +1,40 @@
 class Solution {
 //     Memoization approach dp 
     public int steps(int[] dp,int n){
-        if(n<=1) return 1;
-        if(dp[n]!=0) return dp[n];
+        if(n<=1 && n>=0) return 1 ;
+        if(dp[n]!=-1) return dp[n];
         return dp[n]=steps(dp,n-1)+steps(dp,n-2);
-        
     }
 //     Tabulation Approach
-      public int steps2(int[] dp,int n){
-      for(int i=0;i<n;i++){
-            if(n<=1) {
-                dp[n]= 1;
-                continue;
-            }
+//       public int steps2(int[] dp,int n){
+//       for(int i=0;i<n;i++){
+//             if(n<=1) {
+//                 dp[n]= 1;
+//                 continue;
+//             }
        
-       dp[n]=steps(dp,n-1)+steps(dp,n-2);
-      }
-          return dp[n];
+//        dp[n]=steps(dp,n-1)+steps(dp,n-2);
+//       }
+//           return dp[n];
         
-    }
-//     optimizing 
-    public int optimize(int n){
-        int a=0,b=1;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum=a+b;
-            a=b;
-            b=sum;
-        }
-        return sum;
-    }
+//     }
+// //     optimizing 
+//     public int optimize(int n){
+//         int a=0,b=1;
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             sum=a+b;
+//             a=b;
+//             b=sum;
+//         }
+//         return sum;
+//     }
     
     public int climbStairs(int n) {
       
-        // int x= steps(dp,n);
-        // int x=steps2(dp,n);
-      int x=optimize(n);
-        return x;
+    int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return steps(dp,n);
         
     }
 }
