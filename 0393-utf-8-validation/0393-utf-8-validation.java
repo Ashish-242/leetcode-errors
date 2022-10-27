16ba0fd07ac466c81c2cc44c3b43b35e65c354ef
Ashish <ash242242@gmail.com>
2022-10-27 15:35:37 +0530

    Time: 8 ms (34.24%), Space: 47.5 MB (70.92%) - LeetHub

---
+++ 0393-utf-8-validation/0393-utf-8-validation.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validUtf8(int[] arr) {
+         int rbytes=0;
+        for(int val:arr){
+            if(rbytes==0){
+                
+           if((val>>5)==0b110){
+                 rbytes=1;
+            }
+            else if((val>>4)==0b1110){
+                 rbytes=2;
+            }else if((val>>3)==0b11110){
+                 rbytes=3;
+            }else if((val>>7)!=0b0){
+                return false;
+            }
+            }else{
+                if((val>>6)==0b10){
+                    rbytes--;
+                }else{
+                    return false;
+                }
+            }
+            
+        }
+        if(rbytes==0) return true;
+        
+        return false;
+    }
+}
