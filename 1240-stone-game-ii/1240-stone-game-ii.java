98d712fd6ccfa98d885d83ad11c33669c95f5148
Ashish <ash242242@gmail.com>
2024-08-20 23:42:39 +0530

    Time: 3 ms (89.81%), Space: 42.4 MB (68.93%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.java
+++ 1240-stone-game-ii/1240-stone-game-ii.java
@@ -1,26 +1,34 @@
 class Solution {
+
+    private int solveMemo(int[] pref, int m, int p, int[][] dp) {
+        // Edge Case
+        if (p + 2 * m >= pref.length) {
+            return pref[p];
+        }
+
+        // step-2: if already Calculated
+        // then just return it
+        if (dp[p][m] > 0) {
+            return dp[p][m];
+        }
+
+        // step-3: if not calculated
+        // then calculated and store it in the DP[][]
+        int res = 0, take = 0;
+        for (int i = 1; i <= 2 * m; i++) {
+            take = pref[p] - pref[p + 1];
+            res = Math.max(res,
+                    take + pref[p + 1] - solveMemo(pref, Math.max(i, m), p + i, dp));
+        }
+        return dp[p][m] = res;
+    }
+
     public int stoneGameII(int[] piles) {
-        int n=piles.length;
-        int[][] dp=new int[n][n+1];
-        
-        int[] suffixsum=new int[n];
-        suffixsum[n-1]=piles[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffixsum[i]=piles[i]+suffixsum[i+1];
-
+        int[] pref = Arrays.copyOf(piles, piles.length);
+        for (int i = pref.length - 2; i >= 0; i--) {
+            pref[i] += pref[i + 1];
         }
 
-        for(int i=n-1;i>=0;i--){
-            for(int m=1;m<=n;m++){
-                if(i + 2*m >= n)
-                dp[i][m]=suffixsum[i];
-                else{
-                    for(int x=1;x<=2*m;x++){
-                        dp[i][m]=Math.max(dp[i][m],suffixsum[i]- dp[i+x][Math.max(m,x)]);
-                    }
-                }
-            }
-        }
-        return dp[0][1];
+        return solveMemo(pref, 1, 0, new int[piles.length][piles.length]);
     }
 }
