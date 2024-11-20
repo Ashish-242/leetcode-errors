077872a25d8eec011d00e0bcda022383a1ca317d
Ashish <ash242242@gmail.com>
2024-11-20 22:25:06 +0530

    Time: 112 ms (5.26%), Space: 60.6 MB (55.65%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,38 +1,40 @@
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
-        int n=temp.length;
-       int[] ans=new int[n];
-        
+        int[] ans=new int[temp.length];
         Stack<Integer> st=new Stack<>();
-        
-        for(int i=n-1;i>=0;i--){
-            if(i==n-1){
-                st.push(i);
-              ans[i]=0;
-                continue;
+        Stack<Integer> greatertemp=new Stack<>();
+        for(int i=temp.length-1;i>=0;i--){
+            int num=temp[i];
+            if(st.isEmpty()){
+                st.add(num);
+                greatertemp.add(0);
+                ans[i]=0;
             }
-                int check=0;
-                while(!st.isEmpty() && temp[st.peek()]<= temp[i]){
-                    check+=ans[st.peek()];
-                    st.pop();
-                    
-                }
-                if(st.isEmpty()){
-                    st.push(i);
-                    check=0;
-                    ans[i]=0;
-                }
-                else if(check==0) {
+            else if(st.peek() > num){
+                st.add(num);
+                greatertemp.add(1);
                 ans[i]=1;
-                  st.push(i);  
-                }else{
-                    st.push(i);
-                    ans[i]=check+1;
+            }else if(st.peek() <= num){
+                int sum=1;
+                while(!st.isEmpty() && st.peek()<=num){
+                    st.pop();
+                    sum+=greatertemp.pop();
                 }
-          
+                    if(st.isEmpty()){
+                     greatertemp.add(0);
+                ans[i]=0;
+                }else{
+                    greatertemp.add(sum);
+                ans[i]=sum;
+                }
+                st.add(num);
             
+                
+            }else{
+                st.add(num);
+                greatertemp.add(0);
+            }
         }
         return ans;
-        
     }
 }
