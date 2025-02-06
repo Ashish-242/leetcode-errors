92a57d0e199228a1ff8780c3adefa4c579b43568
Ashish <ash242242@gmail.com>
2025-02-07 00:20:15 +0530

    Time: 2 ms (70.95%), Space: 42.6 MB (48.85%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,59 +1,55 @@
-class Solution {
-    class Pair{
-        int r;
-        int c;
-        Pair(int r,int c){
-            this.r=r;
-            this.c=c;
-        }
+class Pair{
+    int s;
+    int e;
+   
+    Pair(int st,int en){
+        s=st;
+        e=en;
+        
     }
+}
+class Solution {
     public int orangesRotting(int[][] grid) {
-        int count=0;
-        int n=grid.length;
-        int m=grid[0].length;
-        int fresh=0;
+        int fresh=0,minutes=0;
         Queue<Pair> q=new LinkedList<>();
-        boolean[][] visited=new boolean[n][m];
-        int[][] dir=new int[][]{{-1,0},{0,-1},{1,0},{0,1}};
+        int[][] dir=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+        boolean[][] visited=new boolean[grid.length][grid[0].length];
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2 && visited[i][j]==false){
-                   q.add(new Pair(i,j));
-                }else if(grid[i][j]==1){
-                    fresh++;
-                }
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==2) {
+                    q.add(new Pair(i,j));  
+                }else if(grid[i][j]==1) fresh++;
             }
         }
-        
+       
         while(!q.isEmpty()){
-            int s=q.size();
-           
-            while(s-->0){
+            int size=q.size();
+            while(size-->0){
                 Pair p=q.poll();
-                if(visited[p.r][p.c]==true) continue;
-                 visited[p.r][p.c]=true;
-                 
-                if(grid[p.r][p.c]==1) fresh--;
-                for(int i=0;i<dir.length;i++){
-                    int x=p.r+dir[i][0];
-                    int y=p.c+dir[i][1];
-                    if(isvalid(x,y,grid,visited)){
-                        q.add(new Pair(x,y));
-                       
-                    }
+             if(visited[p.s][p.e]==true) continue;
+                 visited[p.s][p.e]=true;
+              if(grid[p.s][p.e]==1) fresh--;
+              for(int i=0;i<dir.length;i++){
+                int start=p.s+dir[i][0];
+                int end=p.e+dir[i][1];
+
+                if(isvalid(start,end,grid,visited)){             
+                 q.add(new Pair(start,end));
                 }
+              }
+             
             }
             if(fresh==0) break;
-             else count++;
-            
+          minutes++;
+             
         }
-        if(fresh==0) return count;
-       
+        if(fresh==0)
+        return minutes;
         return -1;
     }
-    public boolean isvalid(int i,int j,int[][] grid,boolean[][] visited){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || visited[i][j]|| grid[i][j]==0 || grid[i][j]==2) return false;
-        return true;
+    public boolean isvalid(int st,int en,int[][] grid,boolean[][] visited){
+    if(st>=0 && en>=0 && st<grid.length && en<grid[0].length && !visited[st][en] && grid[st][en]!=0) return true;
+    return false;
+}
 
-    }
 }
