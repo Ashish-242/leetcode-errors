fbb6bfe4693a8d53bd5543a1459f630d31b9b1fa
Ashish <ash242242@gmail.com>
2025-03-29 17:20:19 +0530

    Time: 126 ms (6.54%), Space: 46.4 MB (5.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,24 +1,23 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length==1) return 0;
+        if(nums[0]==0) return 0;
+        
         int n=nums.length;
         int[] dp=new int[n+1];
-        //Arrays.fill(dp,-1);
-        dp[n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            int val=nums[i];
-            int mn=Integer.MAX_VALUE-2;
-            for(int j=1;j<=val;j++){
-                if(i+j<dp.length){
-                mn=Math.min(mn,1+dp[i+j]);
-                }
-            }
-            
-            dp[i]=mn;
+        Arrays.fill(dp,-1);
+        int ans=solve(nums,dp,0,0);
+        return ans;
+    }
+    public int solve(int[] nums,int[] dp,int st,int count){
+        if(st>=nums.length ) return Integer.MAX_VALUE-2;
+        if(st== nums.length-1) return dp[st]=0;
+        if(dp[st]!=-1) return dp[st];
+        int val=nums[st];
+        int mn=Integer.MAX_VALUE-2;
+        for(int i=1;i<=val;i++){
+            int value=1+solve(nums,dp,st+i,count);
+          mn=Math.min(mn,value);
         }
-        for(int i=0;i<dp.length;i++){
-            System.out.println(dp[i]);
-        }
-        return dp[0];
+        return dp[st]=mn;
     }
 }
