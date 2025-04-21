dd2e07e3dadf47ec7647de67a9bab6d7c8c69b46
Ashish <ash242242@gmail.com>
2025-04-22 00:03:41 +0530

    Time: 17 ms (10.6%), Space: 45.8 MB (6.22%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,14 +1,16 @@
 class Solution {
     public String countAndSay(int n) {
         if(n==1) return "1";
-        int i=2;
-        String[] r=new String[n+1];
-        r[1]="1";
-        while(i<=n){
-            String val=r[i-1];
-            char[] ch=val.toCharArray();
-            int countno=1;
+        String val=countAndSay(n-1);
+        String res=getrle(val);
+         return res;
+        
+        
+    }
+    public String getrle(String ss){
+            char[] ch=ss.toCharArray();
             String s="";
+            int countno=1;
             for(int j=1;j<ch.length;j++){
                 if(ch[j]==ch[j-1]) countno+=1;
                 else{
@@ -19,10 +21,8 @@ class Solution {
             }
             s+=countno;
             s+=ch[ch.length-1];
-            r[i]=s;
-            i++;
+            return s;
         }
-    return r[r.length-1];
-    }
+   
   
 }
